213732d5eed3b16afb7c5d3eebab63ebdb9dbe28
Marcin Kolak <marcin.kuler@gmail.com>
2019-05-04 10:49:58 +0200

    Spring security with form authentication

---
+++ src/main/java/pl/itacademy/configuration/SecSecurityConfig.java
@@ -0,0 +1,12 @@
+package pl.itacademy.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource({ "classpath:webSecurityConfig.xml" })
+public class SecSecurityConfig {
+    public SecSecurityConfig() {
+        super();
+    }
+}
